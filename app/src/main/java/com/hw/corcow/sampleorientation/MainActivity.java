0dee3e1e1a9a355705bc8ca2bd5dc8bba2e3e2ac
= <cho7673@gmail.com>
2015-10-14 14:47:51 +0900

    Initial commit

---
+++ app/src/main/java/com/hw/corcow/sampleorientation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hw.corcow.sampleorientation;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+
+        
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "Changed portrait", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Changed landscape", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
